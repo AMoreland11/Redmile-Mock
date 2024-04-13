b322a8fe8cf991055e00162854fc5fde1ac1780d
AMoreland11 <a11daaron@gmail.com>
2024-04-13 11:53:07 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const scrollOffset = 60;
+const sections = document.querySelectorAll(".animate-section");
+
+const sectionInView = (section, offset = 0) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    return (
+        sectionTop <=
+        (window.innerHeight || document.documentElement.clientHeight) - offset
+    );
+};
+
+
+const handleScrollAnimation = () => {
+    sections.forEach(section => {
+        if (sectionInView(section, scrollOffset)) {
+            section.classList.add("scrolled");
+        } else {
+            section.classList.remove("scrolled");
+        }
+    });
+};
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+});
